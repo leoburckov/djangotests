0f7adeeb7fc67f8634c5d99abb0c2abba5868704
leoburckov <burckovleo@gmail.com>
2025-10-08 01:54:41 +0400

    done

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
-from . import views
-from .views import contacts, home
+from .views import home, contacts
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', home, name='home'),
-    path('contacts/', contacts, name='contacts'),
+    path('', home, name='home'),           # → http://127.0.0.1:8000/
+    path('contacts/', contacts, name='contacts'),  # → http://127.0.0.1:8000/contacts/
 ]
--- config/settings.py
+++ config/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'catalog/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
