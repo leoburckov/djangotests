6aa25014c86f072edfea78f69bfc8c164c28a14d
leoburckov <burckovleo@gmail.com>
2025-10-22 21:57:26 +0400

    fixes

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from catalog.views import product_detail, home
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalog.urls', namespace="catalog")),  # подключение без префикса
+    path('', include('catalog.urls', namespace="catalog")),
+    path('', home, name='home'),
+    path('product/<int:pk>/', product_detail, name='product_detail'),
 ]
+
