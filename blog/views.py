5301ccb3d43d2f827191c3367167d23e97e2f3b7
leoburckov <burckovleo@gmail.com>
2025-10-29 18:58:39 +0400

    fixes

--- blog/views.py
+++ blog/views.py
@@ -5,22 +5,15 @@ from .models import BlogPost
 
 class BlogPostListView(ListView):
     model = BlogPost
-    template_name = 'blogpost_list.html'
-    context_object_name = 'posts'
+    template_name = 'blog/blogpost_list.html'
 
     def get_queryset(self):
         return BlogPost.objects.filter(is_published=True)
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Блог'
-        return context
-
 
 class BlogPostDetailView(DetailView):
     model = BlogPost
-    template_name = 'blogpost_detail.html'
-    context_object_name = 'post'
+    template_name = 'blog/blogpost_detail.html'
 
     def get_object(self, queryset=None):
         obj = super().get_object(queryset)
@@ -28,44 +21,24 @@ class BlogPostDetailView(DetailView):
         obj.save()
         return obj
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = self.object.title
-        return context
-
 
 class BlogPostCreateView(CreateView):
     model = BlogPost
-    template_name = 'blogpost_form.html'
+    template_name = 'blog/blogpost_form.html'
     fields = ['title', 'content', 'preview', 'is_published']
     success_url = reverse_lazy('blog:post_list')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = 'Создание записи'
-        return context
 
 
 class BlogPostUpdateView(UpdateView):
     model = BlogPost
-    template_name = 'blogpost_form.html'
+    template_name = 'blog/blogpost_form.html'
     fields = ['title', 'content', 'preview', 'is_published']
 
     def get_success_url(self):
         return reverse_lazy('blog:post_detail', kwargs={'pk': self.object.pk})
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = f'Редактирование: {self.object.title}'
-        return context
-
 
 class BlogPostDeleteView(DeleteView):
     model = BlogPost
-    template_name = 'blogpost_confirm_delete.html'
+    template_name = 'blog/blogpost_confirm_delete.html'
     success_url = reverse_lazy('blog:post_list')
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['title'] = f'Удаление: {self.object.title}'
-        return context
