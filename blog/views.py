8fadf8f1721fee79e21cbfe43e0704c737ca5924
leoburckov <burckovleo@gmail.com>
2025-11-05 01:09:49 +0400

    fixes

--- blog/views.py
+++ blog/views.py
@@ -6,21 +6,17 @@ from .models import BlogPost
 class BlogPostListView(ListView):
     model = BlogPost
     template_name = 'blog/blogpost_list.html'
-    fields = ['title', 'content', 'preview', 'is_published']
-    success_url = reverse_lazy('blog:post_list')
     context_object_name = 'posts'
 
     def get_queryset(self):
-        return BlogPost.objects.filter(is_published=True)  # Добавьте этот метод
+        return BlogPost.objects.filter(is_published=True)
 
-    def form_valid(self, form):
-        # Автоматически публиковать новые посты
-        form.instance.is_published = True
-        return super().form_valid(form)
 
 class BlogPostDetailView(DetailView):
     model = BlogPost
     template_name = 'blog/blogpost_detail.html'
+    context_object_name = 'post'
+
 
     def get_object(self, queryset=None):
         obj = super().get_object(queryset)
@@ -34,6 +30,11 @@ class BlogPostCreateView(CreateView):
     template_name = 'blog/blogpost_form.html'
     fields = ['title', 'content', 'preview', 'is_published']
     success_url = reverse_lazy('blog:post_list')
+
+    def form_valid(self, form):
+        # Автоматически публиковать новые посты
+        form.instance.is_published = True
+        return super().form_valid(form)
 
 
 class BlogPostUpdateView(UpdateView):
