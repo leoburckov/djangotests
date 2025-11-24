4eb6a79a4b16dc268531ab0131fa7441eee9e0d3
leoburckov <burckovleo@gmail.com>
2025-11-24 19:15:49 +0400

    fixes

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,21 +1,16 @@
+# catalog/urls.py
 from django.urls import path
 from .views import (
     IndexView, ProductDetailView, ContactsView,
     ProductListView, ProductCreateView, ProductUpdateView,
     ProductDeleteView, ProductModerationView,
-    SafeIndexView  # Добавьте эту строку для временного использования
+    CategoryProductsView  # Добавлен импорт
 )
 
 app_name = 'catalog'
 
 urlpatterns = [
-    # ВРЕМЕННО: используем безопасную версию главной страницы
-    # чтобы избежать ошибок с owner_id до применения миграций
-    path('', SafeIndexView.as_view(), name='home'),
-
-    # ОРИГИНАЛЬНАЯ версия (закомментирована до решения проблемы с миграциями)
-    # path('', IndexView.as_view(), name='home'),
-
+    path('', IndexView.as_view(), name='home'),
     path('product/<int:pk>/', ProductDetailView.as_view(), name='product_detail'),
     path('contacts/', ContactsView.as_view(), name='contacts'),
     path('products/', ProductListView.as_view(), name='product_list'),
@@ -23,4 +18,6 @@ urlpatterns = [
     path('product/<int:pk>/update/', ProductUpdateView.as_view(), name='product_update'),
     path('product/<int:pk>/delete/', ProductDeleteView.as_view(), name='product_delete'),
     path('product/<int:pk>/moderate/', ProductModerationView.as_view(), name='product_moderation'),
+    # Используем ID категории вместо slug
+    path('category/<int:category_id>/', CategoryProductsView.as_view(), name='category_products'),
 ]
